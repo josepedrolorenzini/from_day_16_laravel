9714ce49f873559d8e4651b538f7fdfb6b4157b3
josepedrolorenzini <joseplorenzini@gmail.com>
2024-09-09 11:56:40 +1000

    day 16 laracast laravel

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+
+    return view('jobs.index', [
+        'jobs' => $jobs
+    ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    // validation...
+    //dd(request()->all());
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
